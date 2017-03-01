9323af1ae6783a8fc766f88f186db719feeccd05
emil <emil.digit@gmail.com>
2017-03-01 10:15:06 +0100

    Added house distance

---
+++ Assets/Colliders/HouseDistance.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseDistance: MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 playerpos;
+	private Vector3 housepos;
+
+	public void Start(){
+		housepos = transform.position;
+	}
+
+	public void Update(){
+		playerpos = player.transform.position;
+
+		if (Vector3.Distance (playerpos, housepos) < 10) {
+			if(this.name == "Restaurant")
+				player.GetComponent<PlayerStats> ().isInRestaurant = true;
+			else if (this.name == "Toilet")
+				player.GetComponent<PlayerStats> ().isInRestaurant = true;
+			else if (this.name == "Hotel")
+				player.GetComponent<PlayerStats> ().isInRestaurant = true;
+			else if (this.name == "Refreshments")
+				player.GetComponent<PlayerStats> ().isInRestaurant = true;
+		} else if (Vector3.Distance (playerpos, housepos) > 10) {
+			if(this.name == "Restaurant")
+				player.GetComponent<PlayerStats> ().isInRestaurant = false;
+			else if (this.name == "Toilet")
+				player.GetComponent<PlayerStats> ().isInRestaurant = false;
+			else if (this.name == "Hotel")
+				player.GetComponent<PlayerStats> ().isInRestaurant = false;
+			else if (this.name == "Refreshments")
+				player.GetComponent<PlayerStats> ().isInRestaurant = false;
+		}
+	}
+}
