439fe4c0d5c1e72bb2b54a528abf51ef05d53e95
MarkusBerthold <mbert14@student.aau.dk>
2017-03-01 10:21:51 +0100

    Added toilet moveTo

---
+++ Assets/AI/Actions/MoveToToilet.cs
@@ -0,0 +1,18 @@
+﻿using Apex.AI;
+using UnityEngine;
+
+public sealed class MoveToRestaurant : ActionBase {
+
+	public Vector3 hotelDest = new Vector3 (17, 0, 11);
+
+	public override void Execute(IAIContext context) {
+		// Cast the provided context to your concrete context type
+		var c = (PlayerContext)context;
+
+		var player = c.self;
+
+		player.GetComponent<PlayerStats>().MoveTo(hotelDest);
+
+
+	}
+}
