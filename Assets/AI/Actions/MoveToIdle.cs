6bb59aa1535b212c62f4a0cb475fd713b39046b2
emil <emil.digit@gmail.com>
2017-03-01 10:19:29 +0100

    More updates

---
+++ Assets/AI/Actions/MoveToIdle.cs
@@ -0,0 +1,18 @@
+﻿using Apex.AI;
+using UnityEngine;
+
+public sealed class MoveToIdle : ActionBase {
+
+	public Vector3 idleDest = new Vector3 (0, 0, 0);
+
+	public override void Execute(IAIContext context) {
+		// Cast the provided context to your concrete context type
+		var c = (PlayerContext)context;
+
+		var player = c.self;
+
+		player.GetComponent<PlayerStats>().MoveTo(idleDest);
+
+
+	}
+}
